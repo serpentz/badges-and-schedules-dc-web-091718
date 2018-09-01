21a1b11ecfd191436c856e2e514f83fca5f02f1f
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-01 20:14:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  array = []
+  attendees.each  |person|
+      array.push("Hello, my name is #{person}.")
+    end
+
+end
