ed17a19d7090ae0ab39f2d8728fe3aab11008957
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-01 20:44:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,13 @@
 def badge_maker(name)
 "Hello, my name is #{name}."
 end
-
 def batch_badge_creator(attendees)
   attendees.map  { |person|
       "Hello, my name is #{person}."
+  }
+end
+def assign_rooms(attendees)
+  attendees.map.with_index {|attendee, room_number|
+      "Hello, #{attendee}! You'll be assigned to room #{room_number + 1}!"
   }
 end
