9bfdd6698fa5206aa4607ca5c4320a555bf99fe8
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-01 20:56:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ def assign_rooms(attendees)
   }
 end
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+   batch_badge_creator(attendees).each {|badge|puts badge}
+   assign_rooms(attendees).each {|badge|puts badge}
 end
